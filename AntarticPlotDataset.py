129974cfe382436b7b9779466f00125c23963b63
Arnav Dani <60204322+arnavdani@users.noreply.github.com>
2021-07-21 21:24:26 -0700

    Update AntarticPlotDataset.py

--- AntarticPlotDataset.py
+++ AntarticPlotDataset.py
@@ -1,26 +1,81 @@
 import pandas as pd
+import torchvision.io 
+from torch.utils.data import TensorDataset, DataLoader, Dataset
+import os
+from skimage import io
+import cv2
+import numpy as np  
 
 
-
-
-class AntarticPlotDataset(Dataset):
+class AntarcticPlotDataset(Dataset):
     
     
-    def __init__(self, csv_file, root_dir, transform=None)
+    newdata = []
     
-        self.photoData = open(csv_file)
-        reader = csv.reader(csv_file)
+    def __init__(self, txt_file, root_dir, transform=None):
         
         
-        self.root_dir = 'C:/Users/arnav/OneDrive/Documents/College/Summer 2021/Clarks/MakingEllipse/output_data_set4'
-        
+        self.root_dir = root_dir
         self.transform = transform
+        self.newdata = []
         
-    
+        photoData = txt_file
+        for line in photoData:
+            info = line
+            infolist = info.split(",")
+            finaldata = []
+            imgname = infolist[0] + ".jpg"
+            #print(os.path.join(root_dir, imgname))
+            img = io.imread(os.path.join(root_dir, imgname))
+            img = np.array(img)
+            #cv2.imshow("Image with background removed", img)
+            #cv2.waitKey(0)
+            
+            
+            # print(img)
+            #if (img == None):
+                #print(imgname)
+                #break
+            #else:
+                #print("image is loaded")
+            
+            
+            
+            
+            
+            
+            finaldata.append(img)
+            
+            for i in range(9):
+                
+                finaldata.append(infolist[i+1].split("-")[1].strip('\n'))
+                
+            
+            self.newdata.append(finaldata)
+            #print(len(finaldata))
+            #print(finaldata)
+
+            
+            
+                    
     def __len__(self):
-        return len(self.photoData)
+        return len(self.newdata)
     
     
-    def __getitem__(self, i)
+    def __getitem__(self, i):
+        
+         #cut off all except 0th index
+         if (i < 0 or i > len(self.newdata)):
+            print("problem")
+            return None
+        
+         else:       
+            img = self.newdata[i][0]
+            #landmarks = self.newdata.iloc[i, 1:]
+            self.newdata[i].pop(0)
+            landmarks = self.newdata[i]
+            sample = {'image': img, 'landmarks': landmarks}
+            return sample
+
     
     
