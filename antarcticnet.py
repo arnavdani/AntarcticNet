352a394adfc3f4455e4f79fd6de87ae681959f3a
Arnav Dani <60204322+arnavdani@users.noreply.github.com>
2021-07-26 22:25:51 -0700

    Update antarcticnet.py

--- antarcticnet.py
+++ antarcticnet.py
@@ -24,21 +24,31 @@ class AntarcticNet(nn.Module):
         #bias is default true
         
         self.conv1 = nn.Conv2d(3, 10, kernel_size = 3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size = 3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(10, 16, kernel_size = 3, stride=1, padding=1)
         
-        self.conv3 = nn.Conv2d(20, 40, kernel_size = 3,stride=1, padding=1)
-        self.conv4 = nn.Conv2d(40, 60, kernel_size = 3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(16, 32, kernel_size = 3,stride=1, padding=1)
+        self.conv4 = nn.Conv2d(32, 44, kernel_size = 3, stride=1, padding=1)
         
-        self.conv5 = nn.Conv2d(60, 96, kernel_size = 3, stride=1, padding=1)
-        self.conv6 = nn.Conv2d(96, 144, kernel_size = 3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(44, 72, kernel_size = 3, stride=1, padding=1)
+        self.conv6 = nn.Conv2d(72, 96, kernel_size = 3, stride=1, padding=1)
         
         #for stack 4
         
-        self.conv7 = nn.Conv2d(144, 244, kernel_size = 3, stride=1, padding=1)
-        self.conv8 = nn.Conv2d(244, 244, kernel_size = 3, stride =1, padding=1)
+        self.conv7 = nn.Conv2d(96, 128, kernel_size = 3, stride=1, padding=1)
+        self.conv8 = nn.Conv2d(128, 144, kernel_size = 3, stride =1, padding=1)
         
-        self.lin = nn.Linear(in_features=16, out_features=10)
-        self.lin2 = nn.Linear(in_features = 10, out_features=1)
+        #for stack 5
+        
+        self.conv9 = nn.Conv2d(144, 196, kernel_size = 3, stride=1, padding=1)
+        self.conv10 = nn.Conv2d(196, 196, kernel_size = 3, stride =1, padding=1)
+        
+        
+        # lin conversion
+        
+        
+        self.lin = nn.Linear(in_features=25088, out_features=1024)
+        self.lin2 = nn.Linear(in_features = 1024, out_features=128)
+        self.lin3 = nn.Linear(in_features = 128, out_features=1)
         
         self.pool = nn.MaxPool2d(kernel_size = 3, stride=(2,2), padding=1)
         
@@ -61,13 +71,21 @@ class AntarcticNet(nn.Module):
         x = self.conv6(x)
         x = torch.relu(self.pool(x))
 
-        #out stack
+        #stack 4
         x = self.conv7(x)
         x = self.conv8(x)
         x =  self.pool(x)
-        #x = torch.flatten(x)
+        
+        
+        #stack 5
+        
+        x = self.conv9(x)
+        x = self.conv10(x)
+        x = self.pool(x)
+        x = torch.flatten(x)
         x = self.lin(x)
-        return torch.relu(self.lin2(x))
+        x = self.lin2(x)
+        return self.lin3(x)
     
     
 
